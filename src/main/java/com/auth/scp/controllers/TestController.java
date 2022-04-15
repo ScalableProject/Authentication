92431e2d3de7c2ecdeb4e9314cf5de6de4783864
Abubakar <abubakargill538@gmail.com>
2022-04-15 21:06:23 +0100

    test cicd

--- src/main/java/com/auth/scp/controllers/TestController.java
+++ src/main/java/com/auth/scp/controllers/TestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Testing CICD
+
+
 // test class to verify authentication and authorization 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
